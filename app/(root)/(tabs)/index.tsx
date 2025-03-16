bd3cd2059d4f02246728477d391c05da9f80e547
msrjr91 <michael.s.ross10@gmail.com>
2025-03-16 15:42:24 -0400

    created expo project

---
+++ app/(root)/(tabs)/index.tsx
@@ -0,0 +1,20 @@
+import { Text, View } from "react-native";
+import { Link } from "expo-router";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Link href='/sign-in'>Sign In</Link>
+      <Link href='/booking'>Book Session</Link>
+      <Link href='/sessions'>My Sessions</Link>
+      <Link href='/profile'>Profile</Link>
+      <Link href='/properties/1'>Properties</Link>
+    </View>
+  );
+}
---
+++ app/(root)/(tabs)/sessions.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Sessions = () => {
+  return (
+    <View>
+      <Text>Sessions</Text>
+    </View>
+  )
+}
+
+export default Sessions
